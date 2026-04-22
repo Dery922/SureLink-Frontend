1e5d528cb54c06b6179e4db96faac8a6fe3e0178
Dery922 <dery922@gmail.com>
2026-04-22 10:11:16 +0000

    Icons bug fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Home from './Pages/Home';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function App() {
   return (
